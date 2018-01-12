c5a2042da6d0b7979089490ff3f4494b1cd45748
github username <github email address>
2018-01-11 21:08:17 -0500

    attempting to remake backend

--- jobfound_api/app/controllers/application_controller.rb
+++ jobfound_api/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
-class ApplicationController < ActionController::API
+
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def encode(payload)
+    payload = {
+      payload: payload,
+      exp: 4.hours.from_now.to_i,
+      iat: Time.now.to_i
+    }
+    JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+  end
+
+  def decode(token)
+    options = {
+      verify_iss: true,
+      verify_iat: true,
+      leeway: 30,
+      algorithm: 'HS256'
+    }
+    JWT.decode token, ENV['JWT_SECRET'], true, options
+  end
+  
 end
--- jobfound_api/config/routes.rb
+++ jobfound_api/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
+  root to: 'users#index'
+  resources :notes
+  resources :assets
+  resources :stages
+  resources :contacts
+  resources :companies
+  resources :users
+  resources :applications
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
